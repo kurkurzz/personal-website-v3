b4eadfe9462a4922dbe8deb618d9006315a2258a
hafiz <hafiz@enygma.ai>
2024-02-14 17:22:14 +0800

    add nitro config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,6 +31,13 @@ export default defineNuxtConfig({
 			],
 		}
 	},
+	nitro: {
+		prerender: {
+			crawlLinks: true,
+			routes: ['/sitemap.xml'],
+			failOnError: false, 
+		}
+	},
 	sitemap: {
 		strictNuxtContentPaths: true,
 	},
